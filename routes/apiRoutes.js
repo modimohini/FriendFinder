20d285e9b54ebe43a7bcc526e36f37e85e96d3a1
Mohini Modi <Mohini.Modi@resmed.com>
2019-08-02 19:45:34 -0700

    routing and server set up

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+var friends = require("../data/friends");
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    })
+
+    app.post("/api/friends", function(req,res){
+
+    var bestMatch = {
+        name:"",
+        photo:"",
+        friendDifference: Infinity
+    };
+        
+    })
+
+
+    
+}
