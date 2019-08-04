1d1d9a3336a03db1ca4ea5179853d485b39b3493
Mohini Modi <Mohini.Modi@resmed.com>
2019-08-04 01:35:41 -0700

    survery

---
+++ data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+    {
+      name: "Kim Jerry",
+      photo: "https://i.kinja-img.com/gawker-media/image/upload/s--jeCHOts7--/c_scale,f_auto,fl_progressive,q_80,w_800/v5hs02vdmagrdjbnrn9t.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Robert Dekota",
+      photo: "https://i.pinimg.com/originals/09/3a/c3/093ac3811dcd4f170d91df02995905fe.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Taylor Smith",
+      photo: "https://timedotcom.files.wordpress.com/2014/10/taylor-swift-02.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Kelly Houston",
+      photo: "https://dazedimg-dazedgroup.netdna-ssl.com/640/0-78-999-666/azure/dazed-prod/1240/5/1245447.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Bradly Copper",
+      photo: "https://www.pinknews.co.uk/wp-content/uploads/2019/05/steven_universe_trans_season_finale_-1024x714.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Michel Angalo",
+      photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg",
+      scores: [
+ 
+
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    }
+  
+  ];
+  module.exports = friends;
+  
