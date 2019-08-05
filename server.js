798ac563d38e15783ab4b529eda11ac90a205131
Mohini Modi <Mohini.Modi@resmed.com>
2019-08-04 22:50:29 -0700

    final project done

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 3081;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
